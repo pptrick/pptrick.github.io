56765d9a35433f5563e873f9b3ffb174f4064133
潘传宇 <2017012268@secoder.net>
2022-01-30 21:07:34 +0800

    Add CDN

--- src/next.config.js
+++ src/next.config.js
@@ -3,5 +3,5 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   reactStrictMode: true,
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/pptrick/pptrick.github.io/' : '',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/pptrick/pptrick.github.io/master' : '',
 }
